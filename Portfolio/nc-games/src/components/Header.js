a8242c82e0b4da9389633fecb817592454d415a2
jayebrady <Jaye.Brady@mdtplt1109.lan>
2022-02-07 15:12:40 +0000

    set up template for homepage

---
+++ Portfolio/nc-games/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="Header">Jaye's House of Games</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ Portfolio/nc-games/src/components/Nav.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+const Nav = () => {
+  //   const [categories, setCategories] = useState([]);
+
+  useEffect(() => {}, []);
+  return <nav className="Nav">Nav</nav>;
+};
+
+export default Nav;
